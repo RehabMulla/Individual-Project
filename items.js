6417ae83242e5d98f891236d69827398354dfef6
Rehab Mulla <rehab.mulla@gmail.com>
2022-11-08 20:10:28 +0400

    added all 10 lessons

--- items.js
+++ items.js
@@ -17,4 +17,76 @@ let items = [
         availableSlot: 10, //was 5
         rating: 4
     },
+    {
+        id: 103,
+        subject: "Biology",
+        location: "Abu Dhabi" , //em tag to emphasize the term irrisistible
+        price: 1234,
+        Image: "images/bio.jpg",
+        availableSlot: 15, //was 5
+        rating: 2
+    },
+    {
+        id: 104,
+        subject: "Physics",
+        location: "Mumbai" , //em tag to emphasize the term irrisistible
+        price: 7040,
+        Image: "images/phy.jpg",
+        availableSlot: 13, //was 5
+        rating: 3
+    },
+    {
+        id: 105,
+        subject: "Chemistry",
+        location: "Wakanda" , //em tag to emphasize the term irrisistible
+        price: 1230,
+        Image: "images/chem.jpg",
+        availableSlot: 45, //was 5
+        rating: 2
+    },
+    {
+        id: 106,
+        subject: "History",
+        location: "Dubai" , //em tag to emphasize the term irrisistible
+        price: 5600,
+        Image: "images/history.jpg",
+        availableSlot: 23, //was 5
+        rating: 3
+    },
+    {
+        id: 107,
+        subject: "Economics",
+        location: "Ajman" , //em tag to emphasize the term irrisistible
+        price: 4560,
+        Image: "images/eco.jpg",
+        availableSlot: 56, //was 5
+        rating: 5
+    },
+    {
+        id: 108,
+        subject: "Geography",
+        location: "Ukraine" , //em tag to emphasize the term irrisistible
+        price: 4740,
+        Image: "images/geo.jpg",
+        availableSlot: 21, //was 5
+        rating: 4
+    },
+    {
+        id: 109,
+        subject: "Political Science",
+        location: "Maldives" , //em tag to emphasize the term irrisistible
+        price: 7990,
+        Image: "images/polsci.jpg",
+        availableSlot: 12, //was 5
+        rating: 5
+    },
+    {
+        id: 110,
+        subject: "Computer Science",
+        location: "Rome" , //em tag to emphasize the term irrisistible
+        price: 10040,
+        Image: "images/compsci.jpg",
+        availableSlot: 100, //was 5
+        rating: 4
+    },
     ]
