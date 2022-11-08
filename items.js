0d562a880b8fad921bbb6bb02d00930cefe0c1ba
Rehab Mulla <rehab.mulla@gmail.com>
2022-11-08 19:04:22 +0400

    creating lessons

---
+++ items.js
@@ -0,0 +1,20 @@
+let items = [
+    {
+        id: 101,
+        subject: "Math",
+        location: "Dubai" , //em tag to emphasize the term irrisistible
+        price: 2130,
+        Image: "images/math.jpg",
+        availableSlot: 10, //was 5
+        rating: 3
+    },
+    {
+        id: 101,
+        subject: "Advanced Math",
+        location: "Hendon" , //em tag to emphasize the term irrisistible
+        price: 2160,
+        Image: "images/advmath.jpg",
+        availableSlot: 10, //was 5
+        rating: 4
+    },
+    ]
